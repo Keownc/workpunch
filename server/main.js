b4f2df5d7f10ea5113ac877f4978e1becea310bd
Keownc <kcreese@fullsail.edu>
2016-05-24 14:42:56 -0400

    Start server

--- server/main.js
+++ server/main.js
@@ -1,1 +1,21 @@
 'use-strict'
+
+const express = require('express');
+const body_parser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Adding body parser to parse json automatically
+app.use(body_parser.json());
+
+// Routes
+// app.use('/', require('../routes/js')(express));
+// app.use('/route', require('../routes/js')(express));
+
+// Start server
+var server = app.listen(port, function() {
+    console.log("Listening on " + port + "...");
+});
+
+module.exports = server;
