48d37a2dccc3469789a0e42ca3fcc84dedc3b3bd
Keownc <kcreese@fullsail.edu>
2016-07-12 07:35:49 -0400

    Added passport setup

--- routes/api.js
+++ routes/api.js
@@ -30,4 +30,52 @@ router.post('/register', function(req, res){
     // res.redirect('/employeeDashboard');
 })
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Employee.findOne({ 'username': username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//Login Route
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/employeeDashboard',
+    failureRedirect: '/',
+    failureFlash: true
+    }),
+    function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    req.user;
+    res.redirect('/');
+  });
+
+//log out
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.send(200);
+  res.redirect('/');
+});
+// Employee Dashboard Route
+
+
+
 module.exports = router;
