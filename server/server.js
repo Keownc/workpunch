77e8bbdf7a7aee6f17672e06305679fdae8ee951
Keownc <kcreese@fullsail.edu>
2016-07-11 21:02:20 -0400

    Deleted the api, auth and server files and started new.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 'use-strict'
 const express = require('express');
-var session = require('express-session');
+const session = require('express-session');
 const flash = require('connect-flash');
 const cookie_parser = require('cookie-parser');
 const body_parser = require('body-parser');
@@ -14,53 +14,12 @@ require('../models/model.js');
 require('../models/companyModel.js');
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/workpunch");
+mongoose.connect("mongodb://localhost/pasport");
 const MongoDBStore = require('connect-mongodb-session')(session)
 // Run Server
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-//set static folder
-app.use(express.static(path.join(__dirname, '../public')));
-//Get facicon
-//app.use(facicon(__dirname + '/public/facicon.ico'))
-//Get Index route
-app.get('*', function(req, res) {
-    // res.sendFile(path.join(__dirname, '../public/views', 'index.html'));
-    res.sendFile('index.html', { root: path.join(__dirname, '../public/views') });
-});
-
-// middleware
-app.use(logger('dev'));
-app.use(cookie_parser());
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({extended: false}));
-//express session
-app.use(session({
-    secret: 'punchme cat',
-    saveUninitialized: true,
-    resave: true,
-    store: new MongoDBStore({
-        uri: 'mongodb://localhost:27017/workpunch',
-        collection: 'mySessions',
-        connection: mongoose.connect
-        // ttl: 2 * 3 * 60 * 60
-  })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-//initialize Password
-const init_passport = require('./passport');
-init_passport(passport);
-//connect flash
-app.use(flash());
-// Api routes
-const api = require('../routes/api');
-const auth = require('../routes/auth')(passport);
-app.use('/auth', auth);
-app.use('/api', api);
-
-app.use(upload);
 
 // Start server
 var server = app.listen(port, function() {
